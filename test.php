d5efc6d1a4c7c886bdb60d5fef9a5d408ae5e90a
ferntyp <70442698+ferntyp@users.noreply.github.com>
2021-01-04 15:41:19 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,25 @@
+<?php
+
+$db_handel = pg_connect("host=localhost dbname=car user=postgres password=postgres ");
+
+if ($db_handel) {
+
+echo 'Connection attempt succeeded.';
+
+} else {
+
+echo 'Connection attempt failed.';
+
+}
+
+echo $_GET["id"];
+echo $_GET["status"];
+
+$val = $_GET['id'];
+$va = $_GET['status'];
+
+pg_exec("INSERT INTO public.park(id,status) VALUES ($val,$va)");
+
+pg_close($db_handel);
+
+?>
